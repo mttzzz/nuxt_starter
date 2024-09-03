37fb4fb7d365a1b3514a74f44bf217200e9794e4
Kirill Egorov <kirill_egorov@me.com>
2024-09-03 16:41:53 +0300

    delete 1 file and update 1 file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
+  experimental: { typedPages: true },
   modules: ['@bg-dev/nuxt-naiveui', '@unocss/nuxt', '@pinia/nuxt', '@nuxt/eslint'],
 
   imports: {
