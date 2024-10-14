7e7de8910ae543cb07c38a3df55e7fd4aac4cba2
Kirill Egorov <kirill_egorov@me.com>
2024-10-14 09:54:30 +0300

    update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,18 +5,16 @@ import Components from 'unplugin-vue-components/vite'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   typescript: { typeCheck: true },
+  experimental: { viewTransition: true },
+  devtools: { enabled: true },
+  sourcemap: { server: false, client: false },
   ssr: true,
-  devtools: { enabled: false },
-  experimental: { typedPages: true, viewTransition: true },
   modules: ['@unocss/nuxt', '@pinia/nuxt', '@nuxt/eslint', 'nuxtjs-naive-ui', 'nuxt-cron'],
-  cron: {
-    runOnInit: false,
-    timeZone: 'Europe/Minsk',
-    jobsDir: 'cron',
-  },
-  pinia: {
-    storesDirs: ['./stores/**'],
-  },
+  cron: { runOnInit: false, timeZone: 'Europe/Minsk', jobsDir: 'cron' },
+  pinia: { storesDirs: ['./stores/**'] },
+  eslint: { config: { standalone: false } },
+  compatibilityDate: '2024-08-13',
+  future: { compatibilityVersion: 4 },
   vite: {
     plugins: [
       AutoImport({
@@ -38,16 +36,5 @@ export default defineNuxtConfig({
         dts: '../types/components.d.ts',
       }),
     ],
-  },
-
-  eslint: {
-    config: {
-      standalone: false,
-    },
-  },
-
-  compatibilityDate: '2024-08-13',
-  future: {
-    compatibilityVersion: 4,
   },
 })
